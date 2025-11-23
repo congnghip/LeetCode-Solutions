058aff2a5e579366b78b02537030a392693c38fc
Nghiepdanghoccode <buinguyencongnghiep04@gmail.com>
2025-11-23 22:02:09 +0700

    ReoderList, duyệt đến nữa list sau đảo lại rồi dùng 2 con trỏ sắp vào

---
+++ 143-ReoderList.java
@@ -0,0 +1,42 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+ 
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null)
+        {
+            return;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode preMiddle = slow;
+        ListNode preCurrent = slow.next;
+        while (preCurrent.next != null)
+        {
+            ListNode current = preCurrent.next;
+            preCurrent.next = current.next;
+            current.next = preMiddle.next;
+            preMiddle.next = current;
+        }
+        ListNode p1 = head;
+        ListNode p2 = preMiddle.next;
+        while (p1 != preMiddle)
+        {
+            preMiddle.next = p2.next;
+            p2.next = p1.next;
+            p1.next = p2;
+            p1 = p2.next;
+            p2 = preMiddle.next;
+        }
+    }
+}
